9f713be4fd197a14d4b8d38c45b5cbd9c6794493
Churritzu <churritzu@yahoo.com>
2024-11-14 15:38:12 -0800

    Some config for wezterm.

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font_size = 12
+
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
 config.color_scheme = "Dracula (Official)"
 
 return config
